358a0505aab1ef809dc9893fa341e78240683a04
mastery3 <94207545+mastery3@users.noreply.github.com>
2022-07-15 14:43:47 -0700

    1.19
    1.19

--- src/main/java/mastery3/sharpness6/mixin/UnbreakingMixin.java
+++ src/main/java/mastery3/sharpness6/mixin/UnbreakingMixin.java
@@ -1,7 +1,5 @@
 package mastery3.sharpness6.mixin;
 
-import java.util.Random;
-
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -19,8 +17,8 @@ public class UnbreakingMixin {
 		return Entry.getInstance().config.unbreaking;
 	}
 
-	@Inject(method="shouldPreventDamage(Lnet/minecraft/item/ItemStack;ILjava/util/Random;)Z", at=@At("HEAD"), cancellable=true)
-	private static void shouldPreventDamage(ItemStack item, int level, Random random, CallbackInfoReturnable<Boolean> cir) {
+	@Inject(method="shouldPreventDamage", at=@At("HEAD"), cancellable=true)
+	private static void /*Z*/ shouldPreventDamage(ItemStack item /*Lnet/minecraft/ItemStack;*/, int /*I*/ level, net.minecraft.util.math.random.Random /*Ljava/util/Random;*/ random, CallbackInfoReturnable<Boolean> cir) {
 		if (item.getItem() instanceof ArmorItem) {
 			cir.setReturnValue(Boolean.valueOf(random.nextDouble()*((level + 1)/2) >= 1));
 		 } else {
